68998d5b7bf4d90d223f799106438fe20caef2d3
lalit-003 <gahlawatl7@gmail.com>
2020-10-08 10:14:39 +0530

    allowing choose to symbol X or O

--- Ticyacte/src/com/TictacGame/TicTacToeGame.java
+++ Ticyacte/src/com/TictacGame/TicTacToeGame.java
@@ -1,8 +1,18 @@
 package com.TictacGame;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
 
 	static  char[] board = new char[10];
+	private static char cross ='X';
+	private static char zero ='O';
+	private static char chooseSymbol;
+	private static char playerSymbol;
+	private static char computerSymbol;
+	
+
+	
 	
 	 //creating board char array and intialising it//
 	 public static char[]  creatingBoard() {
@@ -14,10 +24,39 @@ public class TicTacToeGame {
 		return board;
 
 	}
+	 
+	 //allowing player to choose symbol
+	 private static void allowPlayerToChoose()
+	 {
+		 System.out.println("Enter the symbol X or O you want to choose");
+		 Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		chooseSymbol = input.charAt(0);
+		
+		if(chooseSymbol == cross)
+		{
+			playerSymbol = cross;
+			computerSymbol = zero;
+		}
+		else if(chooseSymbol == zero)
+		{
+			playerSymbol = zero;
+			computerSymbol = cross;
+		}
+		else
+		{
+			System.out.println("Invalid input");
+		}
+
+		 
+	 }
+	 
 
 	public static void main(String args[]) {
 
 		System.out.println("Welcome to tic tac board");
 		board = creatingBoard();
+		
+		allowPlayerToChoose();
 	}
 }
