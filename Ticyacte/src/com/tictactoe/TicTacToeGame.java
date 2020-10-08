5e18d5a68262aa26a438251c24f351b40840354f
lalit-003 <gahlawatl7@gmail.com>
2020-10-08 09:46:10 +0530

    master created and made  final changes in uc1-creatingboard

---
+++ Ticyacte/src/com/tictactoe/TicTacToeGame.java
@@ -0,0 +1,12 @@
+package com.tictactoe;
+
+public class TicTacToeGame {
+
+	
+	public static void main(String args[]) {
+
+		System.out.println("welcome to tic tac board");
+
+//		board = creatingBoard();
+	}
+}
