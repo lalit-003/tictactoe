e7520f788037f068d2fda03751b51adee6331732
lalit-003 <gahlawatl7@gmail.com>
2020-10-08 09:02:08 +0530

    Board Created and assigned initially

---
+++ Ticyacte/src/Tictac/ticTacToeGame.java
@@ -0,0 +1,24 @@
+package Tictac;
+
+public class ticTacToeGame
+{
+
+	public void Creating_TicTacToeBorad_UC1()
+	{
+		
+	
+	char[] board = new char[10];
+	
+	for(int i=0;i<board.length;i++)
+	{
+		board[i]= ' ';
+	}
+	
+	
+	}
+	
+	public static void main(String args[])
+	{
+		
+	}
+}
